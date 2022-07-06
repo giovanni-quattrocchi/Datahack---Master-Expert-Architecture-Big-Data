0e9bc7a82e3324316c56a8845e77be1e820081e1
giovanni-quattrocchi <giovanni.quattrocchi85@gmail.com>
2022-07-06 15:15:46 +0200

    Add files via upload

---
+++ Practica-Docker/script/mongo-init.js
@@ -0,0 +1,9 @@
+db = db.getSiblingDB('testdb');
+db.cars.insert({name: "Audi", price: 52642});
+db.cars.insert({name: "Mercedes", price: 57127});
+db.cars.insert({name: "Skoda", price: 9000});
+db.cars.insert({name: "Volvo", price: 29000});
+db.cars.insert({name: "Bentley", price: 350000});
+db.cars.insert({name: "Citroen", price: 21000});
+db.cars.insert({name: "Hummer", price: 41400});
+db.cars.insert({name: "Volkswagen", price: 21600});
